0cc743f710cf2553f5d7ef497df5df60c738401a
Renan Bonk <157993439+renanbonk@users.noreply.github.com>
2024-04-28 21:48:12 -0300

    Update Script-1.sql

--- Aula 06/Script-1.sql
+++ Aula 06/Script-1.sql
@@ -5,49 +5,51 @@
 -- 		cadastrando 3 cidades para estados diferentes
 -- consultar as cidades utilizando inner join
 
-drop database if exists Proway;
+-- Dropa a base de dados se ela existir
+drop database if exists proway;
 
+-- Cria a base de dados proway
 create database proway;
 use proway;
 
+-- Cria a tabela estados
 create table estados (
-	id int auto_increment,
-	nome varchar (35) not null,
-	primary key (id)
+    id int auto_increment,
+    nome varchar (35) not null,
+    primary key (id)
 );
 
+-- Insere os estados
 insert into estados (nome) values
-	("Santa Catarina"),
-	("Paraná"),
-	("Rio Grande do sul");
+    ("Santa Catarina"),
+    ("Paraná"),
+    ("Rio Grande do Sul");
+
+-- Exibe os estados
 select id, nome from estados;
 
+-- Cria a tabela cidades
 create table cidades (
-id int auto_increment,
-id_estado int not null,
-nome varchar (35),
-foreign key (id_estado) references estados (id),
-primary key (id)
+    id int auto_increment,
+    id_estado int not null,
+    nome varchar (35),
+    foreign key (id_estado) references estados (id),
+    primary key (id)
 );
 
+-- Insere as cidades
 insert into cidades (nome, id_estado) values
-("Blumenau", 1),
-insert into cidades (nome, id_estado) values
-("Florianopolis",1),
-insert into cidades (nome, id_estado) values
-("Joinville",1),
-insert into cidades (nome, id_estado) values
-("Maringa",2),
-insert into cidades (nome, id_estado) values
-("Curitiba",2),
-insert into cidades (nome, id_estado) values
-("Cascavel",2),
-insert into cidades (nome, id_estado) values
-("Porto alegre",3),
-insert into cidades (nome, id_estado) values
-("Gramado",3),
-insert into cidades (nome, id_estado) values
-("Pelotas",3);
+    ("Blumenau", 1),
+    ("Florianopolis",1),
+    ("Joinville",1),
+    ("Maringa",2),
+    ("Curitiba",2),
+    ("Cascavel",2),
+    ("Porto Alegre",3),
+    ("Gramado",3),
+    ("Pelotas",3);
 
-select * from cidades
-inner join estados on (cidades.id_estado = estados.id)
+-- Exibe as cidades e seus estados
+select cidades.*, estados.nome as estado
+from cidades
+inner join estados on cidades.id_estado = estados.id;
